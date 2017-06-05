025e5c448e965f3ebfd26b53aa37a90d6b0993fc
cxy <3421857530@qq.com>
2017-06-05 11:28:40 +0800

    6-5

--- mini54zde/User/ir.c
+++ mini54zde/User/ir.c
@@ -39,12 +39,13 @@ _________________________________________________
 #define TIME_INFRARED_ONE_US            2245         //数据“1”的时间：T1=1.685+0.56=2.245ms
 #define TIME_INFRARED_STOP_US			40560		 //数据结束的时间：TH=40+0.56=40.56ms
 
-typedef enum  {IDLE=1,HEAD,REPEAT,DATA,STOP} irstatus_t; 
+typedef enum  {IDLE=1,HEAD,DATA} irstatus_t; 
 typedef union {uint32_t data;struct {uint8_t address0;uint8_t address1;uint8_t data0;uint8_t data1;};}irdata_t;
 irdata_t ir;
 uint32_t irticks=0,ircount=0,ledcount=0;
 irstatus_t irwork=IDLE;
-uint8_t disp_flag=0;
+uint8_t disp_flag=0,disp=0;
+uint8_t KEY_data = 0;
 // The Timer1 default IRQ, declared in startup_Mini51.s.
 void TMR1_IRQHandler(void)
 {
@@ -95,59 +96,56 @@ void GPIO01_IRQHandler(void)
                         break;
 						
                         case HEAD: 
-							irwork=(irticks>((TIME_INFRARED_HEAD_US+TIME_INFRARED_REPEAT_US)/2)/TIME_INTERRUPT_PERIOD_US)?DATA:REPEAT;
-                                ir.data=0;ircount=0;
+							irwork=(irticks>((TIME_INFRARED_HEAD_US+TIME_INFRARED_REPEAT_US)/2)/TIME_INTERRUPT_PERIOD_US)?DATA:IDLE;
+							if( irwork == DATA)			//BOOT_code
+							{
+								ir.data = 0;
+								ircount = 0;
+								disp_flag = 0;
+							}
+                            else// if( disp_flag == 1)	//REPEAT_code
+							{
+								disp++;
+								disp_flag = 1;
+							}								
                         break;
 						
-						case REPEAT:
-							irwork = HEAD;
-							disp_flag++;
-						break;
-						
+
                         case DATA: 
-//							irwork = ( irticks >( ( TIME_INFRARED_ZERO_US + TIME_INFRARED_REPEAT_US )/ 2 )/TIME_INTERRUPT_PERIOD_US)?IDLE:DATA;
-							irdata=(irticks>((TIME_INFRARED_ZERO_US+TIME_INFRARED_ONE_US   )/2)/TIME_INTERRUPT_PERIOD_US)?1:0;
+							irdata=(irticks>((TIME_INFRARED_ZERO_US+TIME_INFRARED_ONE_US )/2)/TIME_INTERRUPT_PERIOD_US)?1:0;
 								if( ircount < 8 )
 								{
 									ir.address0 >>= 1;
 									ir.address0 |= ( irdata << 7 );
+									disp_flag = 0;
 								}
 								else if( ircount < 16 )
 								{
 									ir.address1 >>= 1;
 									ir.address1 |= ( irdata << 7 );
+									disp_flag = 0;
 								}
 								else if( ircount < 24 )
 								{
 									ir.data0 >>= 1;
 									ir.data0 |= ( irdata << 7 );
+									disp_flag = 0;
 								}
 								else if( ircount < 32 )
 								{
 									ir.data1 >>= 1;
 									ir.data1 |= ( irdata << 7 );
-								}
-								else	
-								{
-									disp_flag=1;
-									irwork = STOP;
+									disp_flag = 0;
 								}
 								ircount++;
+								if( ircount >= 32 )	
+								{
+									irwork = IDLE;
+									disp_flag=1;
+									disp = 0;
+									KEY_data = ir.data0;
+								}
 						break;
-								
-						case	STOP:
-							irwork = REPEAT;
-						break;
-								
-//								ir.data |= ( irdata << 7 );
-//								ircount++;
-//                                ir.data<<=1;ir.data|=irdata;ircount++;
-//                                if(ircount>=32)
-//								{
-//									irwork=IDLE;
-//									disp_flag=1;
-//								}
-//                                break;
                 }  
 		irticks=0; 
 		P1->ISRC = BIT0;
@@ -193,6 +191,7 @@ void GPIO234_IRQHandler(void)
 #if 1
 void IR_test(void)
 {
+	static uint8_t irdata;
     /*Configure P24 for LED control */
  //   GPIO_SetMode(P1, BIT4, GPIO_PMD_OUTPUT);
         
@@ -218,13 +217,91 @@ void IR_test(void)
         {
                 if(disp_flag)
                 {
+//					irdata = ir.data0;
+					switch(KEY_data)
+					{
+						case 0x01:
+							P12 = 1;P13 = 1;P14 = 1;
+							if(disp)
+							{
+								P12 = 0;P13 = 0;P14 = 0;
+							}
+							disp_flag=0;
+						break;
+							
+						case 0x02:
+							P12 = 1;P13 = 1;P14 = 0;
+							disp_flag=0;
+						break;
+						
+						case 0x03:
+							P12 = 1;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+						
+						case 0x04:
+							P12 = 1;P13 = 0;P14 = 0;
+							disp_flag=0;
+						break;
+						
+						case 0x0a:
+							P12 = 0;P13 = 1;P14 = 1;
+							disp_flag=0;
+						break;
+						
+						case 0x0b:
+							P12 = 0;P13 = 1;P14 = 0;
+							disp_flag=0;
+						break;
+												
+						case 0x0c:
+							P12 = 0;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+						case 0x0d:
+							P12 = 0;P13 = 0;P14 = 0;
+							disp_flag=0;
+						break;
+
+						case 0x12:
+							P12 = 1;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+						case 0x13:
+							P12 = 0;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+						case 0x14:
+							P12 = 0;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+						case 0x15:
+							P12 = 0;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+						case 0x18:
+							P12 = 0;P13 = 0;P14 = 1;
+							disp_flag=0;
+						break;
+
+					}
  //                       printf("ir.data=0x%x, ADDR=%04x DATA1=%02x /DATA1=%02x DATA0=%02x \n\r",
 //                                        ir.data,ir.address,ir.data1,0xff-ir.data1,ir.data0);
 //                        if((0xff-ir.data0)==ir.data1){P14^=1;}
 //                        disp_flag=0;
-					if(ir.data0!=0){P14^=1;}
-					disp_flag=0;
+//					if( ir.data0 ==0x14 ){P14^=1;}
+//					disp_flag=0;
                 }
+//				else
+//				{
+//					ir.data0 = 0;
+//					irwork = IDLE;
+//				}
         }
 }
 #endif
